a979097d84214cad8a7358aedf7649eb2a61bdf9
Jong-Ki <mirae91@gmail.com>
2022-04-15 14:34:15 +0900

    Update __init__.py

---
+++ perf_dashboard/__init__.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+window = Tk()
+
+## 이 부분에서 화면을 구성하고 처리 ##
+
+window.mainloop()
+window.title("윈도우 창 연습 jk")
+window.geometry("400*400")
+window.resizable(width = FALSE, height = FALSE)
+
+window.mainloop()
